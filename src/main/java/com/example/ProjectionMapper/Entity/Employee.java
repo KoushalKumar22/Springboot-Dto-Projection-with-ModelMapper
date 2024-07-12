5e0b14532d6c7076ea862abd8c6bcbd61e538118
Koushal Kumar <koushalkumar2224@gmail.com>
2024-07-12 21:07:17 +0530

    Entity

---
+++ src/main/java/com/example/ProjectionMapper/Entity/Employee.java
@@ -0,0 +1,55 @@
+package com.example.ProjectionMapper.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String fname;
+    String lname;
+    int sal;
+    String city;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public int getSal() {
+        return sal;
+    }
+
+    public void setSal(int sal) {
+        this.sal = sal;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
