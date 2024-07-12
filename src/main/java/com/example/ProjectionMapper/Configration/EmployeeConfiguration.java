de959d088379a60d1b6f4d39948ebbe97b3aa4ea
Koushal Kumar <koushalkumar2224@gmail.com>
2024-07-12 21:24:42 +0530

    Configuration

--- src/main/java/com/example/ProjectionMapper/Configration/EmployeeConfiguration.java
+++ src/main/java/com/example/ProjectionMapper/Configration/EmployeeConfiguration.java
@@ -1,12 +1,11 @@
 package com.example.ProjectionMapper.Configration;
 
-import org.hibernate.query.NativeQuery;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class EmployeeConfigration {
+public class EmployeeConfiguration {
 
     @Bean
     ModelMapper mymodel()
